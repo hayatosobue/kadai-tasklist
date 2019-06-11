abc9659002ba2a14474bb89d78e8ad242024e38e
hayato.sobue <hsobue@s-pri.co.jp>
2019-06-11 08:40:31 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,15 @@ Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
 
 // ユーザ登録
-Route::get('sigup', 'Auth\RegisterController@showRegistrationForm')->name('sigup.get');
-Route::post('sigup', 'Auth\RegisterController@register')->name('sigup.post');
+Route::get('sigup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('sigup', 'Auth\RegisterController@register')->name('signup.post');
 
 // ログイン認証
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UserController', ['only' => ['index', 'show']]);
+});
